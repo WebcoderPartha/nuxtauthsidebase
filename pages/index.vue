fb4d7d71b83ca2a4de03593544debbfa631c875e
Parthadeb Mondal <parthadev76@gmail.com>
2023-05-25 21:45:45 +0600

    Login Page design

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,21 @@
       
         <div class="row">
             <div class="col-md-4 mx-auto">
-                <div class="card">
+                <div class="card mt-5">
                     <div class="card-header">
                         <h2>Login page</h2>
                     </div>
                     <div class="card-body">
                         <form>
-                            
+                            <div class="form-group p-3">
+                                <input type="text" class="form-control" placeholder="Username">
+                            </div>  
+                            <div class="form-group p-3">
+                                <input type="password" class="form-control" placeholder="password">
+                            </div>  
+                            <div class="form-group p-3 text-center">
+                                <input type="submit" class="btn btn-primary" value="Log In">
+                            </div>    
                         </form>
                     </div>
                 </div>
